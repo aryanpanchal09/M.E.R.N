0911e93a64b272745cb0d7378c025101371d5f7f
Aryan <Aryantech03@gmail.com>
2024-09-27 10:11:12 +0530

    -

---
+++ React/keeper-app/src/Note.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles1.css';
+function Note() 
+{
+    return(
+        <div className='note'>
+            <h1>This is title</h1>
+            <p>This is content</p>
+        </div>
+    );
+}
+export default Note;
