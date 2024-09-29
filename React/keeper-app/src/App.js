c9861bbfb9a43814254c4a0d7580e3a3d061cdf4
Aryan <Aryantech03@gmail.com>
2024-09-29 10:05:40 +0530

    -

--- React/keeper-app/src/App.js
+++ React/keeper-app/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
+import notes from './notes'
+
+function createNote(notes){
+  return <Note title={notes.title} content={notes.content}/>
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
--- React/keeper-app/src/Note.js
+++ React/keeper-app/src/Note.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './styles1.css';
-function Note() 
+function Note(props) 
 {
     return(
         <div className='note'>
-            <h1>This is title</h1>
-            <p>This is content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
