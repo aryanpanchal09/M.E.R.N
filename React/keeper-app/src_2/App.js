2aadc9e537a2ddab7384e6c002c9e970259b48fa
Aryan <Aryantech03@gmail.com>
2024-10-12 16:32:08 +0530

    -

---
+++ React/keeper-app/src_2/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from './Header';
+import Note from './Note';
+import Footer from './Footer';
+import notes from './notes'
+
+function createNote(notes){
+  return <Note title={notes.title} content={notes.content}/>
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(createNote)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ React/keeper-app/src_2/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+    {
+      id: 1, // Changed 'key' to 'id'
+      title: "Delegation",
+      content: "Q. How many programmers does it take to change?",
+    },
+    {
+      id: 2,
+      title: "Loops",
+      content: "Q. How to keep a programmer in the shower forever.",
+    },
+    {
+      id: 3,
+      title: "Arrays",
+      content: "Q. Why did the programmer quit his job?",
+    },
+    {
+      id: 4,
+      title: "Hardware vs Software",
+      content: "What's the difference between hardware and software?",
+    },
+  ];
+  
+  export default notes;
